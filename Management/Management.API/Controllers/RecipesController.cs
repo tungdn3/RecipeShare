498ddceac37659fd85d8610087923bdd07b5f723
Tung Nguyen <tnguyen52@mantu.com>
2024-04-21 14:40:20 +0700

    swagger ui with implicit flow

---
+++ Management/Management.API/Controllers/RecipesController.cs
@@ -0,0 +1,52 @@
+﻿using Management.Core.Dtos;
+using Management.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Management.API.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("management/[controller]")]
+public class RecipesController : ControllerBase
+{
+    private readonly IRecipeService _recipeService;
+    private readonly IUserService _userService;
+
+    public RecipesController(IRecipeService recipeService, IUserService userService)
+    {
+        _recipeService = recipeService;
+        _userService = userService;
+    }
+
+    [HttpGet(Name = "GetRecipes")]
+    public async Task<IEnumerable<RecipeDto>> Get(string? title = null)
+    {
+        int userId = await _userService.GetCurrentUserId();
+        RecipeDto[] recipes = await _recipeService.Get(userId, title);
+        return recipes;
+    }
+
+    [HttpPost(Name = "CreateRecipe")]
+    public async Task<int> Create([FromBody] RecipeCreateDto dto)
+    {
+        int userId = await _userService.GetCurrentUserId();
+        int recipeId = await _recipeService.Create(userId, dto);
+        return recipeId;
+    }
+
+    [HttpPost("publish", Name = "PublishRecipe")]
+    public async Task<IActionResult> Publish([FromBody] RecipePublishDto dto)
+    {
+        await _recipeService.Publish(dto);
+        return NoContent();
+    }
+
+    [HttpPut("{id}", Name = "UpdateRecipe")]
+    public async Task<IActionResult> Update(int id, [FromBody] RecipeUpdateDto dto)
+    {
+        dto.Id = id;
+        await _recipeService.Update(id, dto);
+        return NoContent();
+    }
+}
---
+++ Management/Management.API/Program.cs
@@ -0,0 +1,81 @@
+using Management.API;
+using Management.Infrastructure.EF;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFE", config =>
+    {
+        config.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+        options.Audience = builder.Configuration["Auth0:Audience"];
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = ClaimTypes.NameIdentifier
+        };
+    });
+
+//builder.Services
+//  .AddAuthorization(options =>
+//  {
+//      options.AddPolicy(
+//        "read:messages",
+//        policy => policy.Requirements.Add(
+//          new HasScopeRequirement("read:messages", "https://dev-recipe-share.com")
+//        )
+//      );
+//  });
+
+builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddCore(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration, builder.Environment.IsDevelopment());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi(app.Configuration);
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ManagementDbContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowFE");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
