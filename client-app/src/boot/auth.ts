1df0ce52eb521a070e437fda4abc4d034ed4da60
Tung Nguyen <tnguyen52@mantu.com>
2024-07-11 10:09:06 +0700

    Fix login redirect

--- client-app/src/boot/auth.ts
+++ client-app/src/boot/auth.ts
@@ -3,7 +3,8 @@ import { boot } from 'quasar/wrappers';
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(({ app }) => {
+export default boot(({ app, router }) => {
+  app.config.globalProperties.$router = router;
   app.use(
     createAuth0({
       domain: process.env.QUASAR_AUTH0_DOMAIN || '',
